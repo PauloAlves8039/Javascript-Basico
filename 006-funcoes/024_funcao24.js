a9f01773db1d0091c3ce510f6a04d6620517e4f8
PauloAlves8039 <pj.alves547@gmail.com>
2019-11-13 14:42:17 -0300

    Adicionando chamadas diferentes de funções

---
+++ 006-funcoes/024_funcao24.js
@@ -0,0 +1,11 @@
+/*
+ * Arquivo: 024_funcao24.js
+ * Autor: Paulo Alves
+ * Descrição: utilizando função auto-invocável (IIFE - Immediately Invoked Function Expression) para exibir mensagem
+ * Data: 13/11/2019
+*/
+
+(function (){
+    console.log('Bem vindo a linguagem Javascript!')
+    console.log('Bons estudos!')
+})()

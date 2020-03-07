d95fd029a1845e2eab4a4e5bb7735f925834626e
PauloAlves8039 <pj.alves547@gmail.com>
2020-03-06 21:12:57 -0300

    Adicionando novos exercícios de coleções

---
+++ 008-colecoes/015-concatenando-arrays.js
@@ -0,0 +1,12 @@
+/**
+ * Arquivo: 015-usando-concat.js
+ * Autor: Paulo Alves
+ * Descrição: usando a função concat para concatenar arrays.
+ * Data: 06/03/2020
+*/
+
+const filhas = ['Sansa Stark', 'Arya Stark']
+const filhos = ['John Snow', 'Brandon Stark']
+const todos = filhas.concat(filhos)
+
+console.log(todos)
